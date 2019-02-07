bfc06eb27084f12f0600c0c4eea9a82519f13e6e
Hrvoje Antic <hrvoje.antic@ericsson.com>
2019-02-07 18:31:18 +0100

    Implement autentification system

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Europe/Zagreb',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        //Centaur 
+        Centaur\CentaurServiceProvider::class,
 
     ],
 
